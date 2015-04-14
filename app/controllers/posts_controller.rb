a454b6e621684a5b6bee1ada6fc0622d62ce97dc
rachel-rocks-at-ruby <rachelb@gmail.com>
2015-04-13 21:47:50 -0700

    Views and controller actions for Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+     @post = Post.new(params.require(:post).permit(:title, :body))
+     if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :new
+     end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Post was updated."
+       redirect_to @post
+     else
+       flash[:error] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
+  resources :posts
+
+  get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
 end
