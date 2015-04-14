58f0cd6f381783e40d2116627d3a765c3dda44a3
rachel-rocks-at-ruby <rachelb@gmail.com>
2015-04-14 10:55:06 -0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://jackhuahua.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'jackhuahua.herokuapp.com' }
 end
