c3bb98c09007ea24a39d29c61a17d5bfe55742be
rachel-rocks-at-ruby <rachelb@gmail.com>
2015-05-29 16:21:52 -0700

    Changed fog version to fix avatar upload issue

--- app/policies/gallery_policy.rb
+++ app/policies/gallery_policy.rb
@@ -1,5 +1,6 @@
 class GalleryPolicy < ApplicationPolicy
   def create?
+    #user is logged-in AND the record belongs to current_user or admin
     user.present? && (record.user == user || user.admin?)
   end
 
