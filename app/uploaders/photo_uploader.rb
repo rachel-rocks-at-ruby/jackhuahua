4d94b1440bf6610048ff4a4ae9357159e7cd9372
rachel-rocks-at-ruby <rachelb@gmail.com>
2015-06-04 10:49:43 -0700

    Styled image galleries

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -25,8 +25,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  process :resize_to_fill => [200, 300]
+  #process :scale => [200, 300]
+   process :resize_to_fit => [600, 900]
   #
   # def scale(width, height)
   #   # do something
