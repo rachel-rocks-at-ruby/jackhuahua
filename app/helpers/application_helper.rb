5574de57c0eefb93fa84b3880ef29aebf0b953fb
rachel-rocks-at-ruby <rachelb@gmail.com>
2016-12-24 15:38:20 -0800

    media query

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
      end
    end
 
-    def markdown_to_html(markdown)
+  def markdown_to_html(markdown)
 	  renderer = Redcarpet::Render::HTML.new
 	  extensions = {fenced_code_blocks: true}
 	  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
